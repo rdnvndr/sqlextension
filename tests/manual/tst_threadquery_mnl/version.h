a99a1bf51df9fbd9c0a9c5bf1d8507aec81db9e3
roand <roand@inbox.ru>
2017-08-06 22:35:08 +0300

    Добавлен файл .gitignore

--- tests/manual/tst_threadquery_mnl/version.h
+++
@@ -1,4 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-#define VER_REV "2017-08-05 17:16:23 +0300 (bc1e3a35)"
-#endif
--- tests/manual/tst_uuidtreemodel_mnl/version.h
+++
@@ -1,4 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-#define VER_REV "2017-08-05 09:40:42 +0300 (3f367298)"
-#endif
